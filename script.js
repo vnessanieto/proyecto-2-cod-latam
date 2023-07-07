a412536bab830c34311a83e82c70c24a326bd491
vnessanieto <vanessanieton@gmail.com>
2023-07-07 11:22:03 -0500

    proyecto 2

---
+++ script.js
@@ -0,0 +1,18 @@
+const CV = document.getElementById("CV");
+
+CV.addEventListener("click", function() {
+  
+  console.log("¡Botón clickeado!");
+});
+
+
+
+
+//
+// Obtén el botón por su id
+const Portafolio = document.getElementById("Portafolio");
+
+Portafolio.addEventListener("click", function() {
+  
+    console.log("¡Botón clickeado!");
+  });
