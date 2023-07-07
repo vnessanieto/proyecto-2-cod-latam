11967546b53eb699c00c85863e345000a5f3ed10
vnessanieto <vanessanieton@gmail.com>
2023-07-07 17:32:51 -0500

    texto

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const menuItems = document.querySelectorAll('nav ul li a');
 menuItems.forEach(function(item) {
  
   item.addEventListener('click', function() {
-    
+
     menuItems.forEach(function(item) {
       item.classList.remove('active');
     });
@@ -28,3 +28,28 @@ menuItems.forEach(function(item) {
     this.classList.add('active');
   });
 });
+
+
+const iconos = document.querySelectorAll('.Contacto a i');
+
+i.forEach(icono => {
+  i.addEventListener('click', () => {
+   
+    console.log('Haz hecho clic en:', i.textContent);
+  });
+});
+
+console.log("¡Hola mundo!")
+let num1 = 50;
+let num2 = 10;
+let saludo = "¡Hola!";
+let nombreCurso = "Código LATAM";
+
+let resultadoSuma = num1 + num2;
+let resultadoResta = num1 - num2;
+let resultadoMultiplicacion = num1 * num2
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(saludo + "Esto es" + nombreCurso);
