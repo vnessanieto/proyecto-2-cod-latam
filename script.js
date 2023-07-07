bec62d80fe2f95d86daab852314922e740367521
vnessanieto <vanessanieton@gmail.com>
2023-07-07 17:21:46 -0500

    texto

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ CV.addEventListener("click", function() {
 });
 
 
-
-
 //
-// Obtén el botón por su id
-const Portafolio = document.getElementById("Portafolio");
 
-Portafolio.addEventListener("click", function() {
-  
-    console.log("¡Botón clickeado!");
+const botonPortafolio = document.getElementById("Portafolio");
+
+botonPortafolio.addEventListener("click", function() {
+  alert("¡Click!");
+});
+
+
+const menuItems = document.querySelectorAll('nav ul li a');
+
+menuItems.forEach(function(item) {
+ 
+  item.addEventListener('click', function() {
+    
+    menuItems.forEach(function(item) {
+      item.classList.remove('active');
+    });
+
+    this.classList.add('active');
   });
+});
